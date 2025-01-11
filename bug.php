7e9809fe4aacc59fbb6417d700db619ef8c8a63e
BugHunter-X <askzenai@gmail.com>
2025-01-11 15:50:05 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,15 @@
+function foo(a, b) {
+  if (a === b) {
+    return true;
+  } else {
+    return false; 
+  }
+}
+
+function bar(a, b) {
+  if (a == b) {
+    return true;
+  } else {
+    return false;
+  }
+}
